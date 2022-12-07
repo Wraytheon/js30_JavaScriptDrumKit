500bbff1778714bd8fa68eb75a3c75c3167e1faa
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2022-12-06 19:13:42 -0500

    added functionality to allow sound to be pressed repeatedly in a row

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ window.addEventListener("keydown", function(e) {
   console.log(e.keyCode)
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"`)
   /* uses attribute selector and templatel literal to select the audio elements that have a data-key value of whatever key is pressed */
+  audio.load()
+  /* figured this out myself: allows a sound to be started before its finished playing so that you can play sounds quickly back-to-back */
   audio.play()
 })
 
