4096482a0fecfc2f7e5cb91afba2c7af704e2bc4
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2022-12-06 19:26:36 -0500

    functioning

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
 window.addEventListener("keydown", function(e) {
   console.log(e.keyCode)
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"`)
-  /* uses attribute selector and templatel literal to select the audio elements that have a data-key value of whatever key is pressed */
+    /* uses attribute selector and templatel literal to select the audio elements that have a data-key value of whatever key is pressed */
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"`)
+  console.log(key)
+  key.classList.add("playing")
   audio.load()
   /* figured this out myself: allows a sound to be started before its finished playing so that you can play sounds quickly back-to-back */
   audio.play()
